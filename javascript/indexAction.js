3a761029f0c7a63ba33c6902c7f147d1b15dd7e8
Vitor Albano <vitor-albano@hotmail.com>
2026-04-16 21:44:35 -0300

    finalizada estrutura de lista, insert e remove

--- javascript/indexAction.js
+++ javascript/indexAction.js
@@ -3,19 +3,50 @@ function insertTask() {
     const selectValue = document.getElementById("prioritySelect").value;
 
     if (inputValue) {
-        const taskToInsert = { taskTitle: inputValue, priority: selectValue }
-        var taskList = localStorage.getItem("taskList");
-
-        if (taskList) {
-            taskList = JSON.parse(taskList);
-        } else {
-            taskList = []
-        }
-        console.log("taskList ", taskList);
+        const taskToInsert = { taskTitle: inputValue, priority: selectValue };
+        let taskList = JSON.parse(localStorage.getItem("taskList")) || [];
 
         taskList.push(taskToInsert);
-        console.log("taskList 2 ", taskList);
-        localStorage.setItem("taskTitle", taskList)
-        console.log("task title ", );
-    } 
+
+        localStorage.setItem("taskList", JSON.stringify(taskList));
+
+        document.getElementById("taskTitle").value = "";
+        renderTasks();
+    } else {
+        alert("Digite o título da tarefa!");
+    }
 }
+
+function renderTasks() {
+    const listContainer = document.getElementById("taskListDisplay");
+    const taskList = JSON.parse(localStorage.getItem("taskList")) || [];
+    listContainer.innerHTML = "";
+
+    taskList.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.className = `prioridade-${task.priority}`;
+
+        li.innerHTML = `
+            <div class="task-info">
+                <strong>${task.taskTitle}</strong> 
+                <span>Prioridade ${task.priority}</span>
+            </div>
+            <button class="remove-btn" onclick="removeTask(${index})" title="Remover Tarefa">
+                <img src="../assets/trashIcon.svg" alt="Remover" class="trash-icon">
+            </button>
+        `;
+
+        listContainer.appendChild(li);
+    });
+}
+
+function removeTask(index) {
+    let taskList = JSON.parse(localStorage.getItem("taskList")) || [];
+
+    taskList.splice(index, 1);
+
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+    renderTasks();
+}
+
+window.onload = renderTasks;
